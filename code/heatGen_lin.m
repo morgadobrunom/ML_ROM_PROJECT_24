aec538e1c4d8217dd1cef7021e6b667a4c6df243
morgadobrunom <143545441+morgadobrunom@users.noreply.github.com>
2024-04-02 13:36:11 -0400

    initial code
    initial main code in matlab is uploaded

---
+++ code/heatGen_lin.m
@@ -0,0 +1,6 @@
+function fout = heatGen_lin(region,state,Eresults,rho,thickness, alpha)
+xq = region.x;
+yq = region.y;
+[gradx,grady] = evaluateGradient(Eresults,xq,yq);
+J_sq = (gradx./rho).^2 + (grady./rho).^2;
+fout = thickness*rho*(1 + alpha*state.u).*(J_sq)';
